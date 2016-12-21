5a7d5852abe0853510a2466c962180b5cf8791a2
Grant Ridder <shortdudey123@gmail.com>
2016-12-20 17:38:35 -0800

    Update examples with v2 changes

--- examples/shifts-with-incidents-and-log-entries.rb
+++ examples/shifts-with-incidents-and-log-entries.rb
@@ -3,11 +3,10 @@
 require 'dotenv'
 Dotenv.load ".env.development", '.env'
 
-account = ENV['PAGERDUTY_ACCOUNT'] || raise("Missing ENV['PAGERDUTY_ACCOUNT'], add to .env.development")
 token = ENV['PAGERDUTY_TOKEN'] || raise("Missing ENV['PAGERDUTY_TOKEN'], add to .env.development")
 
 require 'pager_duty/connection'
-$pagerduty = PagerDuty::Connection.new(account, token)
+$pagerduty = PagerDuty::Connection.new(token)
 
 schedule_id = ENV['PAGERDUTY_SCHEDULE_ID'] || raise("Missing ENV['PAGERDUTY_SCHEDULE_ID'], add to .env.development")
 
@@ -15,33 +14,33 @@ schedule_id = ENV['PAGERDUTY_SCHEDULE_ID'] || raise("Missing ENV['PAGERDUTY_SCHE
 time_since = 1.day.ago
 time_until = Time.now
 
-# http://developer.pagerduty.com/documentation/rest/schedules/entries
-response = $pagerduty.get("schedules/#{schedule_id}/entries", 'since' => time_since, 'until' => time_until, :overflow => true)
+# https://v2.developer.pagerduty.com/v2/page/api-reference#!/On-Calls/get_oncalls
+response = $pagerduty.get("oncalls", query_params: { since: time_since, until: time_until, schedule_ids: [schedule_id] })
 
-entries = response['entries'] # note, probably a bug, but response.entries doesn't do what you think it does
+entries = response['oncalls']
 
 entries.each do |entry|
-  puts "#{entry.start} - #{entry.end}: #{entry.user.name}"
+  puts "#{entry['start']} - #{entry['end']}: #{entry['user']['summary']}"
 
   # find incidents during that shift
-  # http://developer.pagerduty.com/documentation/rest/incidents/list
-  response = $pagerduty.get('incidents', :since => entry['start'], :until => entry['end'])
+  # https://v2.developer.pagerduty.com/v2/page/api-reference#!/Incidents/get_incidents
+  response = $pagerduty.get('incidents', query_params: { since: entry['start'], until: entry['end'], user_ids: [entry['user']['id']] })
 
-  response.incidents.each do |incident|
+  response['incidents'].each do |incident|
     puts "\t#{incident.id}"
 
     # find log entries (acknowledged, notifications, etc) for incident:
-    # http://developer.pagerduty.com/documentation/rest/log_entries/incident_log_entries
+    # https://v2.developer.pagerduty.com/v2/page/api-reference#!/Incidents/get_incidents_id_log_entries
     response = $pagerduty.get("incidents/#{incident.id}/log_entries")
 
     # select just the notes
-    notes = response.log_entries.select do |log_entry|
-      log_entry.channel && log_entry.channel.type == 'note'
+    notes = response['log_entries'].select do |log_entry|
+      log_entry['channel'] && log_entry['channel']['type'] == 'note'
     end
 
     # and print them out:
     notes.each do |log_entry|
-      puts "\t\t#{log_entry.channel.summary}"
+      puts "\t\t#{log_entry['channel']['summary']}"
     end
   end
 end
